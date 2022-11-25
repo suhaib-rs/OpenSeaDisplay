a47ebe91207fa5465a5370d8541fed286d73a99d
Suhaib Ahmed <suhaib@Suhaibs-MacBook-Pro-2.local>
2022-11-25 00:04:50 -0500

    Fixed double rendering due to offset being a part of the state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
+var offset = 0
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      collections: [],
-      offset: 0,
+      collections: []
     }
   }
 
@@ -16,17 +16,17 @@ class App extends Component {
     this.getCollections();
   }
   next = () => {
-    this.setState({offset: this.state.offset+5});
+    offset += 5;
     this.getCollections();
   }
   previous = () => {
-    this.setState({offset: this.state.offset-5});
+    offset -= 5;
     this.getCollections();
   }
 
   //Function to get the Customer Data from json
   getCollections() {
-    axios.get(`https://api.opensea.io/api/v1/collections?offset=${this.state.offset}&limit=5`)
+    axios.get(`https://api.opensea.io/api/v1/collections?offset=${offset}&limit=5`)
     .then(response => {
       this.setState({collections: response.data.collections})
     });
@@ -37,7 +37,7 @@ class App extends Component {
       return (<div>Loading...</div>);
     
     let buttons;
-    if (this.state.offset >= 5) {
+    if (offset >= 5) {
       buttons = <div>
       <button onClick={this.previous}>Previous</button>
       <button onClick={this.next}>Next</button>
