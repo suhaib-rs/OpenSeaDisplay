e33a6a3cf97bb4c4d283158ca8befad9706b1c0a
Suhaib Ahmed <suhaib@Suhaibs-MacBook-Pro-2.local>
2022-11-24 22:41:35 -0500

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      collections: [],
+      offset: 0,
+    }
+  }
+
+  //function which is called the first time the component loads
+  componentDidMount() {
+    this.getCollections();
+  }
+  next = () => {
+    this.setState({offset: this.state.offset+5})
+    this.getCollections()
+  }
+  previous = () => {
+    this.setState({offset: this.state.offset-5})
+    this.getCollections()
+  }
+
+  //Function to get the Customer Data from json
+  getCollections() {
+    axios.get(`https://api.opensea.io/api/v1/collections?offset=${this.state.offset}&limit=5`)
+    .then(response => {
+      this.setState({collections: response.data.collections})
+    })
+  };
+
+  render() {
+    if (this.state.collections.length == 0)
+      return (<div>
+        Loading...
+        </div>)
+    
+    return (<div>
+
+
+<button onClick={this.previous}>Previous</button>
+        <button onClick={this.next}>Next</button>
+      {
+      this.state.collections.map(c => {
+      return (<div class="nft" key={c.name}>
+         <p>{c.name}</p>
+         <img src={c.image_url}></img>
+        </div>)}
+        )
+      }
+    </div>)
+  }
 }
 
 export default App;
